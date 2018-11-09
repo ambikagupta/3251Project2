7ba6cbdae56890cb2dae5685d489c8e7a1e8b115
Sahithi Puligundla <spaqua10@gmail.com>
2018-11-09 10:51:51 -0500

    Messages sent as chars

--- Client.java
+++ Client.java
@@ -7,7 +7,7 @@ class Client {
 
 	public static void main(String args[]) throws Exception {
 		String userGuess;
-		String serverMsg;
+		String serverMsg = "";
 
 		String serverIP = args[0];
 		int serverPort = Integer.parseInt(args[1]);
@@ -46,57 +46,124 @@ class Client {
 
     // send multiplayer or single player
     if(yn.equals("y") || yn.equals("Y")) {
-      out.writeBytes("2\n"); // send 2 signaling multiplayer
+      //out.writeBytes("2\n"); // send 2 signaling multiplayer
+			char c = getChar(2);
+			String ss = "" + c;
+			out.writeBytes(ss);
     } else {
-      out.writeBytes("0\n"); // send 0 signaling single player
+      //out.writeBytes("0\n"); // send 0 signaling single player
+			char c = getChar(0);
+			String ss = "" + c;
+			out.writeBytes(ss);
     }
 
     // GAME LOOP GOOD LUCK
-    while(myBool) {
-      serverMsg = in.readLine(); //read packet from server
+    // while(myBool) {
+    //   serverMsg = in.readLine(); //read packet from server
+		//
+    //   // IF GAME PACKET
+    //   if(serverMsg.charAt(0) == '0') {
+    //     parts = serverMsg.split(""); // msg flag, word length, num incorrect, data
+    //     numIncorrect = Integer.parseInt(parts[2]);
+    //     wordLength = Integer.parseInt(parts[1]);
+    //     numBlanks = 0;
+		//
+    //     // print out the word to guess
+    //     for (int i = 3; i < 3 + wordLength; i++) {
+    //       System.out.print(parts[i] + " ");
+    //       // count the number of blanks left
+    //       if (parts[i].equals("_")) {
+    //         numBlanks++;
+    //       }
+    //     }
+		//
+    //     System.out.println();
+		//
+    //     // format and print out incorrect guesses
+    //     incorrectGuesses = "";
+    //     for (int i = 3 + wordLength; i < parts.length; i++) {
+    //       incorrectGuesses = incorrectGuesses + parts[i] + " ";
+    //     }
+    //     System.out.println("Incorrect Guesses: " + incorrectGuesses);
+    //     System.out.println("");
+		//
+    //     //Print out letter guess and get user input for letter quess
+    //     if (numBlanks != 0 && numIncorrect < 6) {
+    //       System.out.print("Letter to Guess: ");
+    //       userGuess = inUser.readLine();  // read in user's guess
+    //       userGuess = checkInput(userGuess, inUser, numIncorrect, wordLength, parts); //check the input given by user
+    //       out.writeBytes("1" + userGuess + "\n"); // send user's guess to server
+    //     }
+		//
+    //     // if message
+    //   } else {
+    //     System.out.println(serverMsg);
+    //     if (serverMsg.contains("Game Over!") || serverMsg.contains("server-overload")) {
+    //       myBool = false;
+    //       s.close();
+    //     }
+    //   }
+    // }
 
-      // IF GAME PACKET
-      if(serverMsg.charAt(0) == '0') {
-        parts = serverMsg.split(""); // msg flag, word length, num incorrect, data
-        numIncorrect = Integer.parseInt(parts[2]);
-        wordLength = Integer.parseInt(parts[1]);
-        numBlanks = 0;
+		while(myBool) {
 
-        // print out the word to guess
-        for (int i = 3; i < 3 + wordLength; i++) {
-          System.out.print(parts[i] + " ");
-          // count the number of blanks left
-          if (parts[i].equals("_")) {
-            numBlanks++;
-          }
-        }
+			if(in.ready() == true) {
+	      int msg = in.read(); //read packet from server
 
-        System.out.println();
+	      // IF GAME PACKET
+	      if(msg == 0) {
+	        wordLength = in.read();
+	        numIncorrect = in.read();
+	        numBlanks = 0;
 
-        // format and print out incorrect guesses
-        incorrectGuesses = "";
-        for (int i = 3 + wordLength; i < parts.length; i++) {
-          incorrectGuesses = incorrectGuesses + parts[i] + " ";
-        }
-        System.out.println("Incorrect Guesses: " + incorrectGuesses);
-        System.out.println("");
+	        // print out the word to guess
+					String toPrint = "";
+	        for (int i = 0; i < wordLength; i++) {
+	          int j = in.read();
+						char str = getChar(j);
+						toPrint = toPrint + str + " ";
+	          // count the number of blanks left
+	          if (str == '_') {
+	            numBlanks++;
+	          }
+	        }
+					System.out.println(toPrint);
 
-        //Print out letter guess and get user input for letter quess
-        if (numBlanks != 0 && numIncorrect < 6) {
-          System.out.print("Letter to Guess: ");
-          userGuess = inUser.readLine();  // read in user's guess
-          userGuess = checkInput(userGuess, inUser, numIncorrect, wordLength, parts); //check the input given by user
-          out.writeBytes("1" + userGuess + "\n"); // send user's guess to server
-        }
+	        System.out.println();
 
-        // if message
-      } else {
-        System.out.println(serverMsg);
-        if (serverMsg.contains("Game Over!") || serverMsg.contains("server-overload")) {
-          myBool = false;
-          s.close();
-        }
-      }
+	        // format and print out incorrect guesses
+	        incorrectGuesses = "";
+	        for (int i = 0; i < numIncorrect; i++) {
+						// int j = in.read();
+						// char str = getChar(j);
+	          incorrectGuesses = incorrectGuesses + getChar(in.read()) + " ";
+	        }
+	        System.out.println("Incorrect Guesses: " + incorrectGuesses);
+	        System.out.println("");
+
+	        //Print out letter guess and get user input for letter quess
+	        if (numBlanks != 0 && numIncorrect < 6) {
+	          System.out.print("Letter to Guess: ");
+	          userGuess = inUser.readLine();  // read in user's guess
+	          //userGuess = checkInput(userGuess, inUser, numIncorrect, wordLength, parts); //check the input given by user
+						char ch = getChar(1);
+	          out.writeBytes(ch + userGuess); // send user's guess to server
+	        }
+
+	        // if message
+	      } else {
+	        //System.out.println(serverMsg);
+					serverMsg = "";
+					for(int i = 0; i < msg; i++) {
+						serverMsg = serverMsg + getChar(in.read());
+					}
+					System.out.println(serverMsg);
+	        if (serverMsg.contains("Game Over!") || serverMsg.contains("server-overload")) {
+	          myBool = false;
+	          s.close();
+	        }
+	      }
+			}
     }
     s.close();
 	}
@@ -156,14 +223,6 @@ class Client {
     }
     return userGuess;
 
-  }
-
-  public static String getChar_message(String s) {
-    int length = s.length();
-    //char c = (char)(length + '0');
-		char c = (char)length;
-    String s_new = c + s;
-    return s_new;
   }
 
   public static char getChar(int i) {
